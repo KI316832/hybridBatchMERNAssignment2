580cec3fd1f5a8f45bad101fcf0b05de8da772ec
kishore <kishore.apiiit@gmail.com>
2016-05-16 23:25:46 +0530

    Assignment2

---
+++ Assignment2/includes/js/script.js
@@ -0,0 +1,21 @@
+/*
+
+My Custom JS
+============
+
+Author:  Kishore
+Created On: 16 May 2016
+
+
+*/
+
+$(function() {
+
+	$('#alertMe').click(function(e) {
+
+		e.preventDefault();
+
+		$('#successAlert').slideDown();
+
+	});
+});
